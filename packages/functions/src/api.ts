314a251909afd6d37f9012b31a3a7ef1d329b8dd
ioetbc <ioetbc@gmail.com>
2025-04-28 19:51:20 +0100

    add a services package envokable from a lambda

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,27 +1,20 @@
 import { Handler } from "aws-lambda";
-import { db, exhibition } from "@monorepo-template/db";
+import { Greeting } from "@monorepo-template/services";
 
 export const handler: Handler = async (_event) => {
   if (_event.requestContext.http.method === "GET") {
-    const result = await db.select().from(exhibition).execute();
+    const greetingService = new Greeting();
+    const name = _event.queryStringParameters?.name || "User";
+    const greeting = greetingService.sayHello(name);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(result, null, 2),
-    };
-  }
-
-  if (_event.requestContext.http.method === "POST") {
-    // TODO: Implement db insert
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "TODO: Implement db insert" }),
+      body: JSON.stringify({ message: greeting }),
     };
   }
 
   return {
     statusCode: 500,
-    body: JSON.stringify({ error: "Neither GET nor POST" }),
+    body: JSON.stringify({ error: "Unsupported HTTP method" }),
   };
 };
---
+++ packages/services/src/greeting.ts
@@ -0,0 +1,5 @@
+export class Greeting {
+  sayHello(name: string = "World"): string {
+    return `Hello, ${name}!`;
+  }
+}
